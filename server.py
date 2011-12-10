f8cb57c5579c328182b55cf75c339990cf24548b
Seh Hui, Leong <felixleong@gmail.com>
2011-12-11 06:01:59 +0800

    Corrected static file hosting route.

--- server.py
+++ server.py
@@ -13,16 +13,16 @@ from chat.chatroom import ChatRouter
 ROOT = op.normpath(op.dirname(__file__))
 def main(port):
     # Starting Django
-    sys.path.append('/home/felix/Projects/trnserver/frontend')
+    sys.path.append(op.join(ROOT, 'frontend'))
     os.environ['DJANGO_SETTINGS_MODULE'] = 'frontend.settings'
 
     wsgi_app = tornado.wsgi.WSGIContainer(django.core.handlers.wsgi.WSGIHandler())
     tornado_app = tornado.web.Application(
         [
-            (r'/', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
-            (r'/static', tornado.web.StaticFileHandler, dict(path='/home/felix/Projects/trnserver/frontend/static')),
+            (r'/', tornado.web.FallbackHandler, {'fallback': wsgi_app}),
+            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': op.join(ROOT, 'frontend/static')}),
             ChatRouter.route(),
-            (r'.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
+            (r'.*', tornado.web.FallbackHandler, {'fallback': wsgi_app}),
         ],
         flash_policy_port = 843,
         flash_policy_file = op.join(ROOT, 'flashpolicy.xml'),
