b059a66e5ec3eed7446478f15cf031500165df03
hellochar <hellocharlien@hotmail.com>
2013-02-19 23:36:06 -0800

    use heroku

--- frontend/settings.py
+++ frontend/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/hellochar/dev/tornadio-with-django/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -102,12 +102,19 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'frontend.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+#TEMPLATE_DIRS = (
+    ## Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    ## Always use forward slashes, even on Windows.
+    ## Don't forget to use absolute paths, not relative paths.
 
-    '/home/hellochar/dev/tornadio-with-django/frontend/templates',
+    #'/home/hellochar/dev/tornadio-with-django/frontend/templates',
+#)
+
+import os.path
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, "templates"),
+    # here you can add another templates directory if you wish.
 )
 
 INSTALLED_APPS = (
@@ -144,3 +151,6 @@ LOGGING = {
         },
     }
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
